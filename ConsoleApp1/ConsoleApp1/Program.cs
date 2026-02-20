c60cae999affc0c6077cf8c68f01200bd8c585dc
tedi4fun <teodor.yosif@gmail.com>
2026-02-20 17:57:19 +0100

    FirstCommit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿string firstName = "Teodor ";
+string lastName = "Denisov";
+Console.WriteLine(firstName + lastName);
+
+
+
+int x = 6;
+
+double y = 5.5;
+
+double z = x + y;
+
+bool isBojoCool = false;
+if (x>=y  ||isBojoCool)
+{
+    Console.WriteLine(firstName);
+}
+
+
+
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+
+while (x <= 10)
+{
+    x++;
+    Console.WriteLine("bojidaregei");
+}
